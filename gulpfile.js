5a07303d8f55dd88ae43af6dad972fe5b8f190b8
Tvortsa <tvortsa@gmail.com>
2019-06-19 21:30:13 +0300

    materialize, gulp, pug

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const { src, dest } = require('gulp');
+
+function defaultTask(cb) {
+    // поместите код для вашей задачи по умолчанию здесь
+    var node_moduls_path = __dirname + '\\node_modules'
+    var mtrlz_path = node_moduls_path + '\\materialize-css'
+    var mtrlz_css_path = mtrlz_path + '\\dist\\css\\materialize.min.css'
+    var mtrlz_js_path = mtrlz_path + '\\dist\\js\\materialize.js'
+    var mtrlz_min_js_path = mtrlz_path + '\\dist\\js\\materialize.min.js'
+    return src(mtrlz_css_path)
+        .pipe(dest('src\\stylesheets\\'))
+        .pipe(src(mtrlz_js_path))
+        .pipe(dest('src\\javascripts\\'))
+        .pipe(src(mtrlz_min_js_path))
+        .pipe(dest('src\\javascripts\\'))
+    console.log("путь к корню проекта: " + __dirname);
+    console.log("путь к модулям node: " + node_moduls_path);
+
+    cb();
+}
+
+exports.default = defaultTask
