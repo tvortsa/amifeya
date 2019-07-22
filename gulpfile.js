ecdd7af1295f78aa8c4b58c078788aa19137513a
Tvortsa <tvortsa@gmail.com>
2019-07-22 19:01:23 +0300

    Включаю GUI пакет Xel

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,24 @@ function defaultTask(cb) {
     var mtrlz_css_path = mtrlz_path + '\\dist\\css\\materialize.min.css'
     var mtrlz_js_path = mtrlz_path + '\\dist\\js\\materialize.js'
     var mtrlz_min_js_path = mtrlz_path + '\\dist\\js\\materialize.min.js'
+        //xel files
+    var xel_path = node_moduls_path + '\\xel'
+    var xel_themes_path = xel_path + '\\themes'
+    var xel_material_theme = xel_themes_path + '\\material.css'
+    var xel_js = xel_path + '\\xel.min.js'
     return src(mtrlz_css_path)
         .pipe(dest('src\\stylesheets\\'))
         .pipe(src(mtrlz_js_path))
         .pipe(dest('src\\javascripts\\'))
         .pipe(src(mtrlz_min_js_path))
         .pipe(dest('src\\javascripts\\'))
+        //xel files
+        .pipe(src(xel_material_theme))
+        .pipe(dest('src\\stylesheets\\'))
+        .pipe(src(xel_js))
+        .pipe(dest('src\\javascripts\\'))
+
+
     console.log("путь к корню проекта: " + __dirname);
     console.log("путь к модулям node: " + node_moduls_path);
 
